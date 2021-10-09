3743fc9ecf11f7531ae13f26794488670e3653a8
Alexander Annenkov <55015984+axel-hk@users.noreply.github.com>
2021-10-10 01:08:21 +0300

    Fixed test

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,13 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -11,7 +18,10 @@ class JsonCustomClassDeserializer {
     fun `Нобходимо десериализовать данные в класс`() {
         // given
         val data = """{"client": "Иванов Иван Иванович"}"""
+        val simpleModuleClient = SimpleModule()
+            .addDeserializer(Client7::class.java, ClientDeserializer())
         val objectMapper = ObjectMapper()
+            .registerModules(simpleModuleClient)
 
         // when
         val client = objectMapper.readValue<Client7>(data)
@@ -21,4 +31,15 @@ class JsonCustomClassDeserializer {
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
     }
+
+    class  ClientDeserializer: StdDeserializer<Client7>(Client7::class.java){
+        override fun deserialize(p0: JsonParser?, p1: DeserializationContext?): Client7 {
+            val node: JsonNode? = p0?.codec?.readTree(p0)
+            val args = node?.get("client").toString().trim('"').split(" ")
+
+            return Client7(args[1], args[0], args[2])
+        }
+
+    }
+
 }
--- serialization/src/test/kotlin/ru/sber/serialization/JsonSerializationTest.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonSerializationTest.kt
@@ -1,6 +1,9 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -10,7 +13,7 @@ class JsonSerializationTest {
     fun `Не должны сериализовываться свойства с null значениям Настройка через аннотацию`() {
         // given
         val client = Client5()
-        val objectMapper = ObjectMapper()
+        val objectMapper = ObjectMapper().registerModules(KotlinModule(), JavaTimeModule()).setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
         // when
         val data = objectMapper.writeValueAsString(client)
@@ -23,7 +26,7 @@ class JsonSerializationTest {
     fun `Не должны сериализовываться свойства с null значениям Настройка через ObjectMapper`() {
         // given
         val client = Client6()
-        val objectMapper = ObjectMapper()
+        val objectMapper = ObjectMapper().registerModules(KotlinModule(), JavaTimeModule())
 
         // when
         val data = objectMapper.writeValueAsString(client)
